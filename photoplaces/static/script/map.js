7a5f7da03b754128b766862a333f5e6c1b375ac7
Joona Heinikoski <joona@Peruna.local>
2015-01-16 10:37:22 +0200

    Drawing cluster centers on map with D3.

--- photoplaces/static/script/map.js
+++ photoplaces/static/script/map.js
@@ -3,6 +3,7 @@ var bounds = new google.maps.LatLngBounds ();
 var markers = {};
 var cluster_polygons = {};
 var zoomTimeout;
+var cluster_center_overlay;
 var marker_image = {
     url: "/static/images/red_marker.png",
     anchor: new google.maps.Point(4,4)}
@@ -99,7 +100,51 @@ function add_clustering_run_to_map(data){
     });
     bounds = new google.maps.LatLngBounds ();
     cluster_polygons = [];
-    add_cluster_to_map(data.features, 0);
+    cluster_center_overlay = new google.maps.OverlayView();
+    //add_cluster_to_map(data.features, 0);
+
+    cluster_center_overlay.onAdd = function() {
+        var layer = d3.select(this.getPanes().overlayLayer).append("div")
+            .attr("class", "cluster_center");
+
+        cluster_center_overlay.draw = function() {
+            var projection = this.getProjection(),
+                padding = 10;
+
+            var marker = layer.selectAll("svg")
+                .data(data.features)
+                .each(transform)
+                .enter().append("svg:svg")
+                .each(transform)
+                .attr("class", "marker");
+
+            marker.append("svg:polygon")
+                .attr("points", function(d){
+                    var out = [];
+                    for (var j = 0.0; j < 12.0; j += 1.0){
+                        var phase = j / 12.0 * 2 * Math.PI;
+                        out.push([5 + Math.sin(phase) * 4, 5 + Math.cos(phase) * 4]);
+                    }
+                    return out.join(" ");
+                });
+
+            function transform(cluster) {
+                var coords = cluster.geometry.coordinates;
+                var d = new google.maps.LatLng(coords[1], coords[0]);
+                d = projection.fromLatLngToDivPixel(d);
+                return d3.select(this)
+                    .style("left", (d.x - padding) + "px")
+                    .style("top", (d.y - padding) + "px");
+            }
+        };
+        $.each(data.features, function(idx, cluster){
+            var coords = cluster.geometry.coordinates;
+            var d = new google.maps.LatLng(coords[1], coords[0]);
+            bounds.extend(d);
+        });
+        map.fitBounds(bounds);
+    };
+    cluster_center_overlay.setMap(map);
 }
 
 function finalize_clustering_run_to_map(clusters){
